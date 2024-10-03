1d0c924755b9f8f8f43bf6b58d1ed7b1c3588c89
Gabriel <gabrielhenriqueojr@gmail.com>
2024-10-03 18:42:27 -0300

    "Alteração em Pilha.topo"

--- src/Pilha.java
+++ src/Pilha.java
@@ -16,7 +16,11 @@ public class Pilha {
 
     public Bloco topo() {
 
-        return this.lista.localizarBloco(0);
+        return this.lista.localizarBloco(this.lista.tamanho());
+        /*top retorna localizar(tamanho) porque o elemento da fila que ta no topo 
+        é o ultimo, já que é LIFO (last in, first out)
+        return this.lista.localizarBloco(0); tava assim antes
+         */
 
     }
 }
