b89d1db73ec06254a2a86afa81d3c5815321e2b2
PatoDesoxigenado <luizsantosteixeira@gmail.com>
2024-12-08 19:57:53 -0300

    Questão 1

--- src/TerceiraListaEstrutura/ListaDuplamenteEncadeada.java
+++ src/TerceiraListaEstrutura/ListaDuplamenteEncadeada.java
@@ -2,93 +2,75 @@ package TerceiraListaEstrutura;
 
 public class ListaDuplamenteEncadeada {
 
-    public Bloco addBloco(int valor){
-        Bloco novo = new Bloco();
-        cabecalho.tamanho ++;
-        novo.valor = valor;
-        if (this.isVazia()){
-            this.cabecalho.inicio = novo;
-            this.cabecalho.fim = novo;
-        } else {
-            novo.ant = this.cabecalho.fim;
-            this.cabecalho.fim.prox = novo;
-            this.cabecalho.fim = novo;
+    public Bloco inicio;
+
+    public Bloco addBloco(int valor, boolean isInicio){
+
+        if (isInicio) {
+            return addBlocoInicio (valor);
+        }else {
+            return addBlocoFim (valor);
         }
+
+    }
+
+    public Bloco addBlocoInicio (int valor) {
+        Bloco novo = new Bloco();
+        novo.valor = valor;
+
+        if (this.isVazia()) {
+            this.inicio = novo;
+        }else {
+            novo.prox = this.inicio;
+            this.inicio = novo;
+        }
+
         return novo;
     }
 
-    public Bloco localizarFim(int position){  //Percorre a partir do fim
-        Bloco aux = cabecalho.fim;
-        int contador = this.cabecalho.tamanho;
-            while(aux != cabecalho.inicio && contador > position){
-                if (aux != null){
-                    aux = aux.ant;
-                }
-                contador--;
-        }
+    public Bloco addBlocoFim (int valor) {
 
-        return aux;
-    }
-    
-    public ListaDuplamenteEncadeada localizarValor(int valor) {
-        Bloco aux = cabecalho.inicio;
-        ListaDuplamenteEncadeada novaLista = new ListaDuplamenteEncadeada();
-            while (aux != null){
-                if (aux.valor == valor){
-                    novaLista.addBloco(valor);
-                }
+        if (isVazia()) {
+
+            return addBlocoInicio(valor);
+
+        }else {
+            Bloco novo = new Bloco();
+            novo.valor = valor;
+            Bloco aux = this.inicio;
+
+            while (aux.prox != null) {
                 aux = aux.prox;
             }
-            return novaLista;
-        }
 
-    public Bloco removerBloco(int posicao){ //remover começando do fim
-        if(this.cabecalho.fim == null){
-            return null;
+            aux.prox = novo;
+    
+            return novo;
         }
-        if(posicao < 0){
-            return null;
-        }
-        Bloco blocoAlvo = localizarFim(posicao);
-        if(blocoAlvo == this.cabecalho.fim){
-            this.cabecalho.fim = blocoAlvo.ant;
-            if(this.cabecalho.fim != null){
-                this.cabecalho.fim.prox = null;
-            }else{
-                this.cabecalho.inicio = null;
-            }
-        }else if(blocoAlvo == this.cabecalho.inicio){
-            this.cabecalho.inicio = blocoAlvo.prox;
-        }else{
-            Bloco blocoAnterior = blocoAlvo.ant;
-            Bloco blocoProximo = blocoAlvo.prox;
-            blocoAnterior.prox = blocoProximo;
-            if(blocoProximo != null){
-                blocoProximo.ant = blocoAnterior;
-            }
-        }
-        this.cabecalho.tamanho --;
-        blocoAlvo.prox = null;
-        blocoAlvo.ant = null;
-        return blocoAlvo;
+   
     }
+    
+    public Bloco localizarValor(int valor) {
+        Bloco aux = this.inicio;
+        
+            while (aux != null && aux.valor != valor){
+
+                aux = aux.prox;
+
+            }
+
+            return aux;
+        }
 
     public Boolean isVazia(){
-        return this.cabecalho.inicio == null;
+        return this.inicio == null;
     }
 
     public void mostrarLista() {
-        Bloco blocoAux = this.cabecalho.inicio;
+        Bloco blocoAux = this.inicio;
         while (blocoAux != null) {
             System.out.println(blocoAux.valor);
             blocoAux = blocoAux.prox;
         }
-        int tamanho = this.cabecalho.tamanho;
-        if(tamanho > 0) {
-            System.out.println(tamanho + " blocos");
-        }else{
-            System.out.println("Lista vazia");
-        }
     }
-    
 }
